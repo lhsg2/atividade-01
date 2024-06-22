295d5fd17cce3043b5c9e284b4823e6ce8f5d4a3
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 11:27:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+n1 = float(input("degite a primereira nota:"))
+n2 = float(input("degite a segunda nota:"))
+n3 = float(input("degite a terceira nota:"))
+n4 = float(input("degie a quarta nota:"))
+media = (n1+n2+n3+n4)/4
+print(f"sua media:{media}")
